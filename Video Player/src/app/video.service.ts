3f319d69afdbf2b1683541179a7cb9cb2617e4b5
Nisha V Prasad <prasadnishav@gmail.com>
2017-09-01 12:45:43 +0530

    heroku git:clone -a video-player-app

--- Video Player/src/app/video.service.ts
+++ Video Player/src/app/video.service.ts
@@ -6,34 +6,34 @@ import { Video } from './video';
 @Injectable()
 export class VideoService {
 
-  private _getUrl = '/api/https://video-player-app.herokuapp.com/videos';
-  private _postUrl = '/api/https://video-player-app.herokuapp.com/videos';
-  private _putUrl = '/api/https://video-player-app.herokuapp.com/video/';
-  private _deleteUrl = '/api/https://video-player-app.herokuapp.com/video/';
+ // private _getUrl = '/api/https://video-player-app.herokuapp.com/videos';
+ // private _postUrl = '/api/https://video-player-app.herokuapp.com/videos';
+ // private _putUrl = '/api/https://video-player-app.herokuapp.com/video/';
+ // private _deleteUrl = '/api/https://video-player-app.herokuapp.com/video/';
 
   constructor( private _http: Http ) { }
 
     getVideos() {
-      return this._http.get(this._getUrl)
+      return this._http.get('https://video-player-app.herokuapp.com/videos')
       .map((response: Response) => response.json());
     }
 
     addVideo(video: Video) {
       const headers = new Headers({ 'Content-type': 'application/json' });
       const options = new RequestOptions({ headers: headers });
-      return this._http.post(this._postUrl, JSON.stringify(video), options)
+      return this._http.post('https://video-player-app.herokuapp.com/videos', JSON.stringify(video), options)
       .map((response: Response) => response.json());
     }
 
     updateVideo(video: Video) {
       const headers = new Headers({ 'Content-type': 'application/json' });
       const options = new RequestOptions({ headers: headers });
-      return this._http.put(this._putUrl + video._id, JSON.stringify(video), options)
+      return this._http.put('/https://video-player-app.herokuapp.com/video/' + video._id, JSON.stringify(video), options)
       .map((response: Response) => response.json());
     }
 
     deleteVideo(video: Video) {
-      return this._http.delete(this._deleteUrl + video._id)
+      return this._http.delete('https://video-player-app.herokuapp.com/video/' + video._id)
       .map((response: Response) => response.json());
     }
 }
